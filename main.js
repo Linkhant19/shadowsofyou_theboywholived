0086e64604be2e06029695e73972fe06a851a6b6
Lin Khant Ko <lkk19@bu.edu>
2024-03-14 13:39:19 -0400

    finished the first draft of full product I imagined

--- main.js
+++ main.js
@@ -5,13 +5,14 @@ const book = document.querySelector("#book");
 const paper1 = document.querySelector("#p1");
 const paper2 = document.querySelector("#p2");
 const paper3 = document.querySelector("#p3");
+const paper4 = document.querySelector("#p4");
 
 // button event listeners
 prevBtn.addEventListener("click", goPrevPage);
 nextBtn.addEventListener("click", goNextPage);
 
 let currentLocation = 1;
-let numOfPapers = 3;
+let numOfPapers = 4;
 let maxLocation = numOfPapers + 1;
 
 function openBook() {
@@ -43,9 +44,13 @@ function goNextPage() {
                 paper2.classList.add("flipped");
                 paper2.style.zIndex = 2;
                 break;
-            case 3:
+            case 3: 
                 paper3.classList.add("flipped");
                 paper3.style.zIndex = 3;
+                break;
+            case 4:
+                paper4.classList.add("flipped");
+                paper4.style.zIndex = 4;
                 closeBook(false);
                 break;
             default:
@@ -61,16 +66,20 @@ function goPrevPage() {
             case 2: 
                 closeBook(true);
                 paper1.classList.remove("flipped");
-                paper1.style.zIndex = 3;
+                paper1.style.zIndex = 4;
                 break;
             case 3:
                 paper2.classList.remove("flipped");
-                paper2.style.zIndex = 2;
+                paper2.style.zIndex = 3;
                 break;
             case 4:
-                openBook();
                 paper3.classList.remove("flipped");
-                paper3.style.zIndex = 1;
+                paper3.style.zIndex = 2;
+                break;
+            case 5:
+                openBook();
+                paper4.classList.remove("flipped");
+                paper4.style.zIndex = 1;
                 break;
             default:
                 throw new Error("unknown state: PrevPage");
