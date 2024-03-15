2d2a06907be6bc309c3dffd22d4593e6c2cef5a1
Lin Khant Ko <lkk19@bu.edu>
2024-03-14 23:44:03 -0400

    added a way for background to get darker in mobile version

--- docs/main.js
+++ docs/main.js
@@ -93,24 +93,36 @@ function goNextPage() {
                 paper1.classList.add("flipped");
                 paper1.style.zIndex = 1;
                 document.body.style.backgroundColor = darkenColor(39);
+                if (checkOrientationVertical()) {
+                    document.body.style.backgroundImage = "url(background1.jpg)";
+                }
                 document.getElementById('b1').style.backfaceVisibility = 'visible'; 
                 break;
             case 2:
                 paper2.classList.add("flipped");
                 paper2.style.zIndex = 2;
                 document.body.style.backgroundColor = darkenColor(39);
+                if (checkOrientationVertical()) {
+                    document.body.style.backgroundImage = "url(background2.jpg)";
+                }
                 document.getElementById('b2').style.backfaceVisibility = 'visible'; 
                 break;
             case 3: 
                 paper3.classList.add("flipped");
                 paper3.style.zIndex = 3;
                 document.body.style.backgroundColor = darkenColor(39);
+                if (checkOrientationVertical()) {
+                    document.body.style.backgroundImage = "url(background3.jpg)";
+                }
                 document.getElementById('b3').style.backfaceVisibility = 'visible'; 
                 break;
             case 4:
                 paper4.classList.add("flipped");
                 paper4.style.zIndex = 4;
                 document.body.style.backgroundColor = darkenColor(39);
+                if (checkOrientationVertical()) {
+                    document.body.style.backgroundImage = "url(background4.jpg)";
+                }
                 document.getElementById('b4').style.backfaceVisibility = 'visible'; 
                 closeBook(false);
                 break;
@@ -130,24 +142,36 @@ function goPrevPage() {
                 paper1.style.zIndex = 4;
                 document.body.style.backgroundColor = darkenColor(-39);
                 document.getElementById('b1').style.backfaceVisibility = 'hidden'; 
+                if (checkOrientationVertical()) {
+                    document.body.style.backgroundImage = "url('backgroundRotated.jpg')";
+                }
                 break;
             case 3:
                 paper2.classList.remove("flipped");
                 paper2.style.zIndex = 3;
                 document.body.style.backgroundColor = darkenColor(-39);
                 document.getElementById('b2').style.backfaceVisibility = 'hidden'; 
+                if (checkOrientationVertical()) {
+                    document.body.style.backgroundImage = "url(background1.jpg)";
+                }
                 break;
             case 4:
                 paper3.classList.remove("flipped");
                 paper3.style.zIndex = 2;
                 document.body.style.backgroundColor = darkenColor(-39);
                 document.getElementById('b3').style.backfaceVisibility = 'hidden'; 
+                if (checkOrientationVertical()) {
+                    document.body.style.backgroundImage = "url(background2.jpg)";
+                }
                 break;
             case 5:
                 openBook();
                 paper4.classList.remove("flipped");
                 paper4.style.zIndex = 1;
                 document.body.style.backgroundColor = darkenColor(-39);
+                if (checkOrientationVertical()) {
+                    document.body.style.backgroundImage = "url(background3.jpg)";
+                }
                 document.getElementById('b4').style.backfaceVisibility = 'hidden'; 
                 break;
             default:
